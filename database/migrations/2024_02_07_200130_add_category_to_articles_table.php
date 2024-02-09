03d74054ab827b1f059d450f408e0753518c9cc5
Andrés <acrerosj@canariaseducacion.es>
2024-02-09 14:56:16 +0000

    relations Category-Article

--- database/migrations/2024_02_07_200130_add_category_to_articles_table.php
+++ database/migrations/2024_02_07_200130_add_category_to_articles_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->dropForeign('category_id');
         });
     }
 };
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,6 +19,7 @@
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <div class="sm:fixed sm:top-0 sm:left-0 p-6 text-right z-10">
+                <a href="{{ route('category.index') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Category</a>
                 <a href="{{ route('article.index') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Article</a>
                 @if (Route::has('login'))
                     @auth
